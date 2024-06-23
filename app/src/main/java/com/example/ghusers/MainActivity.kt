05e184141b621b17f42fc5277df425603ce574c0
Ivan Shkilev <i.shkilev@cuberto.ru>
2024-06-23 18:16:51 +0200

    + |project| added dependencies

--- app/src/main/java/com/example/ghusers/MainActivity.kt
+++ app/src/main/java/com/example/ghusers/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.ghusers.ui.theme.GHUsersTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
